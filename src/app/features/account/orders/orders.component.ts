4427700c28920e140af35cfcf1e2c7e334cc4ed1
Galih Satrio Wibisono <sgalih1234@gmail.com>
2024-05-31 17:19:43 +0700

    -

--- src/app/features/account/orders/orders.component.ts
+++ src/app/features/account/orders/orders.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GlobalService } from 'src/app/services/global.service';
+import { NgbDate, NgbCalendar, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.scss']
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
+    filter: any = {};
+    hoveredDate: NgbDate | null = null;
+	fromDate: NgbDate;
+	toDate: NgbDate | null = null;
 
+	constructor(
+        private calendar: NgbCalendar,
+		private globalService: GlobalService,
+	) {
+        this.fromDate = calendar.getToday();
+		this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+    }
+    
+    ngOnInit(): void {
+        this.empty();
+    }
+
+    empty() {
+        this.filter = {
+            status: 'All',
+            nama: ''
+        }
+    }
+
+    onDateSelection(date: NgbDate) {
+		if (!this.fromDate && !this.toDate) {
+			this.fromDate = date;
+		} else if (this.fromDate && !this.toDate && date.after(this.fromDate)) {
+			this.toDate = date;
+		} else {
+			this.toDate = null;
+			this.fromDate = date;
+		}
+	}
+
+	isHovered(date: NgbDate) {
+		return (
+			this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate)
+		);
+	}
+
+	isInside(date: NgbDate) {
+		return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+	}
+
+	isRange(date: NgbDate) {
+		return (
+			date.equals(this.fromDate) ||
+			(this.toDate && date.equals(this.toDate)) ||
+			this.isInside(date) ||
+			this.isHovered(date)
+		);
+	}
 }
