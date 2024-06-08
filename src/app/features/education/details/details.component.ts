c5a7ce512b30505390d65f62bc133b42c1ff1952
Galih Satrio Wibisono <sgalih1234@gmail.com>
2024-06-09 01:00:00 +0700

    edukasi

--- src/app/features/education/details/details.component.ts
+++ src/app/features/education/details/details.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GlobalService } from 'src/app/services/global.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+    model: any = [];
+    listCategory: any = [];
+    
+    constructor(
+        private globalService: GlobalService,
+        private route: ActivatedRoute,
+    ) {}
 
+    ngOnInit(): void {
+        this.route.params.subscribe(params => {
+            const slug = params['slug'];
+        });
+    }
 }
--- src/app/features/education/list/list.component.ts
+++ src/app/features/education/list/list.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { GlobalService } from 'src/app/services/global.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
+    loading = {
+        slider: false
+    };
+    listSlider: any;
 
+    constructor(
+        private globalService: GlobalService,
+        private router: Router,
+    ) { }
+
+    ngOnInit(): void {
+        this.getSlider()
+    }
+
+    getSlider() {
+        this.loading.slider = true;
+        this.globalService.DataGet('/public/slider').subscribe((res: any) => {
+            this.listSlider = res.data.list;
+            this.loading.slider = false;
+        })
+    }
+
+    sliderClick(url: string) {
+        if (url !== null || url !== '') {
+            window.open(url, '_blank')
+        }
+    }
 }
