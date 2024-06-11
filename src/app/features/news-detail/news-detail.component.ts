bb4c27664c78cbad3dbc054bd3b301f38d2530c7
hoggykurnia@gmail.com <hoggykurnia@gmail.com>
2024-06-12 02:43:33 +0700

    SEO

--- src/app/features/news-detail/news-detail.component.ts
+++ src/app/features/news-detail/news-detail.component.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GlobalService } from 'src/app/services/global.service';
+import { Meta, Title } from '@angular/platform-browser';
+import Swal from 'sweetalert2';
+import { MetaDataService } from 'src/app/services/meta-data.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-news-detail',
   templateUrl: './news-detail.component.html',
   styleUrls: ['./news-detail.component.scss']
 })
-export class NewsDetailComponent {
+export class NewsDetailComponent extends MetaDataService implements OnInit {
   model: any = [];
+  webUrl = environment.url;
   listCategory: any = [];
 
   constructor(
     private globalService: GlobalService,
     private route: ActivatedRoute,
-  ) { }
+    titleService: Title,
+    metaService: Meta
+  ) {
+    super(titleService, metaService);
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
@@ -27,6 +36,7 @@ export class NewsDetailComponent {
   getNews(slug: string) {
     this.globalService.DataGet(`/public/news/${slug}`).subscribe((res: any) => {
       this.model = res.data.article;
+      this.updateTags(this.model.judul, 'news/' + this.model.slug, this.model.meta_content, this.model.image);
     })
   }
 
@@ -38,5 +48,44 @@ export class NewsDetailComponent {
 
   resolved(captchaResponse) {
     console.log(`Resolved captcha with response: ${captchaResponse}`);
+  }
+
+  copy() {
+    // copy url to clipboard
+    window.navigator.clipboard.writeText(window.location.href);
+    // show alert
+    Swal.fire({
+      text: 'Link News berhasil disalin',
+      icon: 'success',
+      showCancelButton: false,
+      showConfirmButton: false,
+      timer: 2000
+    })
+  }
+
+  clickShareButton(link = 'salin') {
+    let contentShare = this.model.meta_content;
+
+    switch (link) {
+      case 'wa':
+        window.open("https://wa.me/?text=" + encodeURIComponent(contentShare + " " + "\r\n" + this.webUrl + 'news/' + this.model.slug));
+        break;
+      case 'telegram':
+        window.open("https://t.me/share/url?url=" + encodeURIComponent(this.webUrl + 'news/' + this.model.slug) + "&text=" + encodeURIComponent(contentShare));
+        break;
+      case 'facebook':
+        window.open("https://www.facebook.com/sharer.php?u=" + encodeURIComponent(this.webUrl + 'news/' + this.model.slug));
+        break;
+      case 'twitter':
+        window.open("https://twitter.com/share?url=" + encodeURIComponent(this.webUrl + 'news/' + this.model.slug) + "&text=" + encodeURIComponent(contentShare));
+        break;
+      case 'email':
+        let mailtoLink = "mailto:?subject=" + encodeURIComponent(this.model.judul) + "&body=" + encodeURIComponent(this.model.content);
+        window.location.href = mailtoLink;
+        break;
+      default:
+        this.copy();
+        break;
+    }
   }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,14 +1,17 @@
+let url = 'https://jakartacamera.moodstudio.id/'
 let apiURL = 'https://apijakartacamera.moodstudio.id/api/v1';
 let originalImageURL = 'https://apijakartacamera.moodstudio.id/img/media/originals/';
 let productImageURL = 'https://apijakartacamera.moodstudio.id/img/media/product/';
 
 if (window.location.hostname == 'localhost') {
+  url = 'http://localhost:3200/'
   apiURL = 'http://localhost:8000/api/v1';
   originalImageURL = 'http://localhost:8000/img/media/originals/';
   productImageURL = 'http://localhost:8000/img/media/product/';
 }
 
 export const environment = {
+  url: url,
   apiURL: apiURL,
   originalImageURL: originalImageURL,
   productImageURL: productImageURL,
