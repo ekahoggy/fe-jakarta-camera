b0281f4bd5b60f21d4badadc8340f5fd002092c5
Galih Satrio Wibisono <sgalih1234@gmail.com>
2024-06-19 21:15:37 +0700

    news - integrasi api komentar

--- src/app/features/detail-product/detail-product.component.ts
+++ src/app/features/detail-product/detail-product.component.ts
@@ -4,192 +4,192 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { GlobalService } from 'src/app/services/global.service';
 
 @Component({
-  selector: 'app-detail-product',
-  templateUrl: './detail-product.component.html',
-  styleUrls: ['./detail-product.component.scss']
+    selector: 'app-detail-product',
+    templateUrl: './detail-product.component.html',
+    styleUrls: ['./detail-product.component.scss']
 })
 export class DetailProductComponent implements OnInit {
-  desc: string = 'description';
-  configRekomendasi: any = {};
-  model: any = {};
-  listProduct: any = [];
-  listVariant: any = [];
-  varian1: any = '';
-  varian1_detail: any = [];
-  varian2: any = '';
-  varian2_detail: any = [];
-  selectVarian1: any = '';
-  selectVarian2: any = '';
-  activePhoto: string = "";
-  selectedVarian: any = null;
-  loadingPage: boolean = true;
+    desc: string = 'description';
+    configRekomendasi: any = {};
+    model: any = {};
+    listProduct: any = [];
+    listVariant: any = [];
+    varian1: any = '';
+    varian1_detail: any = [];
+    varian2: any = '';
+    varian2_detail: any = [];
+    selectVarian1: any = '';
+    selectVarian2: any = '';
+    activePhoto: string = "";
+    selectedVarian: any = null;
+    loadingPage: boolean = true;
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private globalService: GlobalService,
-    private modalService: NgbModal,
-  ) { }
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private globalService: GlobalService,
+        private modalService: NgbModal,
+    ) { }
 
-  ngOnInit() {
-    this.route.params.subscribe(params => {
-      const slug = params['slug'];
-      this.getProduct(slug);
-      this.getFlashSale();
-    });
-    this.configRekomendasiProduct();
-  }
-
-  switchDesc(params: string) {
-    this.desc = params;
-  }
-
-  onSwiper(swiper: any) { }
-  onSlideChange() { }
-
-  configRekomendasiProduct() {
-    this.configRekomendasi = {
-      slidesPerView: 4,
-      spaceBetween: 20,
-      navigation: true,
-      pagination: false,
-      scrollbar: { draggable: true },
-      breakpoints: {
-        100: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        480: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 20,
-        },
-        1400: {
-          slidesPerView: 4,
-          spaceBetween: 20,
-        },
-      },
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            const slug = params['slug'];
+            this.getProduct(slug);
+            this.getFlashSale();
+        });
+        this.configRekomendasiProduct();
     }
-  }
 
-  getProduct(slug: string) {
-    this.loadingPage = true;
-    this.globalService.DataGet('/public/getProdukSlug', { slug: slug }).subscribe((res: any) => {
-      this.model = res.data;
+    switchDesc(params: string) {
+        this.desc = params;
+    }
 
-      //proses variant
-      this.listVariant = this.model.variant.all_varian;
-      this.varian1 = this.model.variant.varian1;
-      this.varian1_detail = this.model.variant.varian1_detail;
-      this.varian2 = this.model.variant.varian2;
-      this.varian2_detail = this.model.variant.varian2_detail;
+    onSwiper(swiper: any) { }
+    onSlideChange() { }
 
-      if (this.varian1_detail.length > 0) {
-        this.selectVarian1 = this.varian1_detail[0];
+    configRekomendasiProduct() {
+        this.configRekomendasi = {
+            slidesPerView: 4,
+            spaceBetween: 20,
+            navigation: true,
+            pagination: false,
+            scrollbar: { draggable: true },
+            breakpoints: {
+                100: {
+                    slidesPerView: 2,
+                    spaceBetween: 10,
+                },
+                480: {
+                    slidesPerView: 2,
+                    spaceBetween: 10,
+                },
+                768: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                1024: {
+                    slidesPerView: 3,
+                    spaceBetween: 20,
+                },
+                1400: {
+                    slidesPerView: 4,
+                    spaceBetween: 20,
+                },
+            },
+        }
+    }
+
+    getProduct(slug: string) {
+        this.loadingPage = true;
+        this.globalService.DataGet('/public/getProdukSlug', { slug: slug }).subscribe((res: any) => {
+            this.model = res.data;
+
+            //proses variant
+            this.listVariant = this.model.variant.all_varian;
+            this.varian1 = this.model.variant.varian1;
+            this.varian1_detail = this.model.variant.varian1_detail;
+            this.varian2 = this.model.variant.varian2;
+            this.varian2_detail = this.model.variant.varian2_detail;
+
+            if (this.varian1_detail.length > 0) {
+                this.selectVarian1 = this.varian1_detail[0];
+                this.getStok();
+            }
+            if (this.varian2_detail.length > 0) {
+                this.selectVarian2 = this.varian2_detail[0];
+                this.getStok();
+            }
+
+            this.model.quantity = 1;
+            this.activePhoto = this.model.foto;
+            this.loadingPage = false;
+        })
+    }
+
+    pilihVariant1(varian: string) {
+        this.selectVarian1 = varian;
         this.getStok();
-      }
-      if (this.varian2_detail.length > 0) {
-        this.selectVarian2 = this.varian2_detail[0];
+    }
+
+    pilihVariant2(varian: string) {
+        this.selectVarian2 = varian;
         this.getStok();
-      }
-
-      this.model.quantity = 1;
-      this.activePhoto = this.model.foto;
-      this.loadingPage = false;
-    })
-  }
-
-  pilihVariant1(varian: string) {
-    this.selectVarian1 = varian;
-    this.getStok();
-  }
-
-  pilihVariant2(varian: string) {
-    this.selectVarian2 = varian;
-    this.getStok();
-  }
-
-  getStok() {
-    this.model.quantity = 1;
-    this.globalService.DataGet('/public/stok', {
-      m_produk_id: this.model.id,
-      varian1: this.selectVarian1,
-      varian2: this.selectVarian2
-    }).subscribe((res: any) => {
-      this.model.stok = res.data.stok;
-      this.model.harga = res.data.harga;
-      this.selectedVarian = res.data.id;
-    });
-  }
-
-  reduceTotal() {
-    if (this.model.quantity > 1) {
-      this.model.quantity--
     }
-  }
 
-  addTotal() {
-    if (this.model.stok == 1) {
-      this.model.quantity = 1;
+    getStok() {
+        this.model.quantity = 1;
+        this.globalService.DataGet('/public/stok', {
+            m_produk_id: this.model.id,
+            varian1: this.selectVarian1,
+            varian2: this.selectVarian2
+        }).subscribe((res: any) => {
+            this.model.stok = res.data.stok;
+            this.model.harga = res.data.harga;
+            this.selectedVarian = res.data.id;
+        });
     }
-    else if (this.model.stok > 1) {
-      this.model.quantity++;
+
+    reduceTotal() {
+        if (this.model.quantity > 1) {
+            this.model.quantity--
+        }
     }
-  }
 
-  changeQuantity() {
-    if (this.model.quantity === '0') {
-      this.model.quantity = 1;
+    addTotal() {
+        if (this.model.stok == 1) {
+            this.model.quantity = 1;
+        }
+        else if (this.model.stok > 1) {
+            this.model.quantity++;
+        }
     }
-  }
 
-  addCart() {
-    if (this.globalService.getAuth() === null) {
-      this.router.navigate(['/login']);
-    } else {
-      let params = {
-        user_id: this.globalService.getAuth()['user']['id'],
-        product_id: this.model.id,
-        promo_id: this.model.is_promo ? this.model.promo.m_promo_id : null,
-        persen: this.model.is_promo ? this.model.promo.persen : 0,
-        product_varian_id: this.selectedVarian,
-        quantity: this.model.quantity
-      }
-      this.globalService.DataPost('/cart/add', params).subscribe((res: any) => {
-        this.globalService.alertSuccess('Berhasil', res.message);
-      })
+    changeQuantity() {
+        if (this.model.quantity === '0') {
+            this.model.quantity = 1;
+        }
     }
-  }
 
-  addCartFlashSale(productId: string) {
-    if (this.globalService.getAuth() === null) {
-      this.router.navigate(['/login']);
-    } else {
-      let params = {
-        user_id: this.globalService.getAuth()['user']['id'],
-        product_id: productId,
-        quantity: 1
-      }
-      this.globalService.DataPost('/cart/add', params).subscribe((res: any) => {
-        this.globalService.alertSuccess('Berhasil', res.message);
-      })
+    addCart() {
+        if (this.globalService.getAuth() === null) {
+            this.router.navigate(['/login']);
+        } else {
+            let params = {
+                user_id: this.globalService.getAuth()['user']['id'],
+                product_id: this.model.id,
+                promo_id: this.model.is_promo ? this.model.promo.m_promo_id : null,
+                persen: this.model.is_promo ? this.model.promo.persen : 0,
+                product_varian_id: this.selectedVarian,
+                quantity: this.model.quantity
+            }
+            this.globalService.DataPost('/cart/add', params).subscribe((res: any) => {
+                this.globalService.alertSuccess('Berhasil', res.message);
+            })
+        }
     }
-  }
 
-  getFlashSale() {
-    this.globalService.DataGet('/public/produk').subscribe((res: any) => {
-      this.listProduct = res.data.list;
-    })
-  }
+    addCartFlashSale(productId: string) {
+        if (this.globalService.getAuth() === null) {
+            this.router.navigate(['/login']);
+        } else {
+            let params = {
+                user_id: this.globalService.getAuth()['user']['id'],
+                product_id: productId,
+                quantity: 1
+            }
+            this.globalService.DataPost('/cart/add', params).subscribe((res: any) => {
+                this.globalService.alertSuccess('Berhasil', res.message);
+            })
+        }
+    }
 
-  openPreviewPhoto(modal: TemplateRef<any>) {
-    this.modalService.open(modal, { size: 'xl' });
-  }
+    getFlashSale() {
+        this.globalService.DataGet('/public/produk').subscribe((res: any) => {
+            this.listProduct = res.data.list;
+        })
+    }
+
+    openPreviewPhoto(modal: TemplateRef<any>) {
+        this.modalService.open(modal, { size: 'xl' });
+    }
 }
--- src/app/features/news-detail/news-detail.component.ts
+++ src/app/features/news-detail/news-detail.component.ts
@@ -7,85 +7,112 @@ import { MetaDataService } from 'src/app/services/meta-data.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-news-detail',
-  templateUrl: './news-detail.component.html',
-  styleUrls: ['./news-detail.component.scss']
+    selector: 'app-news-detail',
+    templateUrl: './news-detail.component.html',
+    styleUrls: ['./news-detail.component.scss']
 })
 export class NewsDetailComponent extends MetaDataService implements OnInit {
-  model: any = [];
-  webUrl = environment.url;
-  listCategory: any = [];
+    model: any = [];
+    webUrl = environment.url;
+    listCategory: any = [];
+    listComment: any = [];
+    auth: any;
 
-  constructor(
-    private globalService: GlobalService,
-    private route: ActivatedRoute,
-    titleService: Title,
-    metaService: Meta
-  ) {
-    super(titleService, metaService);
-  }
-
-  ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      const slug = params['slug'];
-      this.getNews(slug);
-    });
-    this.getCategory();
-  }
-
-  getNews(slug: string) {
-    this.globalService.DataGet(`/public/news/${slug}`).subscribe((res: any) => {
-      this.model = res.data.article;
-      this.updateTags(this.model.judul, 'news/' + this.model.slug, this.model.meta_content, this.model.image);
-    })
-  }
-
-  getCategory() {
-    this.globalService.DataGet('/public/category-news').subscribe((res: any) => {
-      this.listCategory = res.data.list;
-    })
-  }
-
-  resolved(captchaResponse) {
-    console.log(`Resolved captcha with response: ${captchaResponse}`);
-  }
-
-  copy() {
-    // copy url to clipboard
-    window.navigator.clipboard.writeText(window.location.href);
-    // show alert
-    Swal.fire({
-      text: 'Link News berhasil disalin',
-      icon: 'success',
-      showCancelButton: false,
-      showConfirmButton: false,
-      timer: 2000
-    })
-  }
-
-  clickShareButton(link = 'salin') {
-    let contentShare = this.model.meta_content;
-
-    switch (link) {
-      case 'wa':
-        window.open("https://wa.me/?text=" + encodeURIComponent(contentShare + " " + "\r\n" + this.webUrl + 'news/' + this.model.slug));
-        break;
-      case 'telegram':
-        window.open("https://t.me/share/url?url=" + encodeURIComponent(this.webUrl + 'news/' + this.model.slug) + "&text=" + encodeURIComponent(contentShare));
-        break;
-      case 'facebook':
-        window.open("https://www.facebook.com/sharer.php?u=" + encodeURIComponent(this.webUrl + 'news/' + this.model.slug));
-        break;
-      case 'twitter':
-        window.open("https://twitter.com/share?url=" + encodeURIComponent(this.webUrl + 'news/' + this.model.slug) + "&text=" + encodeURIComponent(contentShare));
-        break;
-      case 'email':
-        let mailtoLink = "mailto:?subject=" + encodeURIComponent(this.model.judul) + "&body=" + encodeURIComponent(this.model.content);
-        window.location.href = mailtoLink;
-        break;
-      default:
-        this.copy();
-        break;
+    constructor(
+        private globalService: GlobalService,
+        private route: ActivatedRoute,
+        titleService: Title,
+        metaService: Meta
+    ) {
+        super(titleService, metaService);
     }
-  }
+
+    ngOnInit(): void {
+        this.auth = this.globalService.getAuth()["user"];
+        this.route.params.subscribe(params => {
+            const slug = params['slug'];
+            this.getNews(slug);
+        });
+        this.getCategory();
+    }
+
+    getNews(slug: string) {
+        this.globalService.DataGet(`/public/news/${slug}`).subscribe((res: any) => {
+            this.model = res.data.article;
+            this.updateTags(this.model.judul, 'news/' + this.model.slug, this.model.meta_content, this.model.image);
+            this.getComment(this.model.id);
+        })
+    }
+
+    getCategory() {
+        this.globalService.DataGet('/public/category-news').subscribe((res: any) => {
+            this.listCategory = res.data.list;
+        })
+    }
+
+    resolved(captchaResponse) {
+        this.model.captcha = captchaResponse;
+        console.log(`Resolved captcha with response: ${captchaResponse}`);
+    }
+
+    copy() {
+        // copy url to clipboard
+        window.navigator.clipboard.writeText(window.location.href);
+        // show alert
+        Swal.fire({
+            text: 'Link News berhasil disalin',
+            icon: 'success',
+            showCancelButton: false,
+            showConfirmButton: false,
+            timer: 2000
+        })
+    }
+
+    clickShareButton(link = 'salin') {
+        let contentShare = this.model.meta_content;
+
+        switch (link) {
+            case 'wa':
+                window.open("https://wa.me/?text=" + encodeURIComponent(contentShare + " " + "\r\n" + this.webUrl + 'news/' + this.model.slug));
+                break;
+            case 'telegram':
+                window.open("https://t.me/share/url?url=" + encodeURIComponent(this.webUrl + 'news/' + this.model.slug) + "&text=" + encodeURIComponent(contentShare));
+                break;
+            case 'facebook':
+                window.open("https://www.facebook.com/sharer.php?u=" + encodeURIComponent(this.webUrl + 'news/' + this.model.slug));
+                break;
+            case 'twitter':
+                window.open("https://twitter.com/share?url=" + encodeURIComponent(this.webUrl + 'news/' + this.model.slug) + "&text=" + encodeURIComponent(contentShare));
+                break;
+            case 'email':
+                let mailtoLink = "mailto:?subject=" + encodeURIComponent(this.model.judul) + "&body=" + encodeURIComponent(this.model.content);
+                window.location.href = mailtoLink;
+                break;
+            default:
+                this.copy();
+                break;  
+        }
+    }
+
+    getComment(id) {
+        this.globalService.DataGet(`/public/news/comment/${id}`).subscribe((res: any) => {
+            this.listComment = res.data.list;
+        })
+    }
+
+    postComment() {
+        const params = {
+            user_id: this.auth.id,
+            news_id: this.model.id,
+            nama: this.model.nama,
+            email: this.model.email,
+            komentar: this.model.komentar
+        }
+        const final = Object.assign(params)
+        this.globalService.DataPost('/public/news/post', final).subscribe((res: any) => {
+            this.globalService.alertSuccess('Berhasil', 'Komentar berhasil di unggah');
+        }, (error:any) => {
+            this.globalService.alertError('Mohon Maaf', error.error.message);
+        });
+    }
 }
