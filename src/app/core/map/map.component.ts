df978929a880fc99513a32f871b4f1f3936406d6
Galih Satrio Wibisono <sgalih1234@gmail.com>
2024-06-25 02:23:09 +0700

    map

--- src/app/core/map/map.component.ts
+++ src/app/core/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import { GlobalService } from 'src/app/services/global.service';
 
 @Component({
     selector: 'app-map',
@@ -7,24 +8,38 @@ import * as L from 'leaflet';
     styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-   
-  constructor() { }
+    query: string = '';
+    results: any[] = [];
+    constructor(private globalService: GlobalService) { }
 
-  ngOnInit(): void {
-    console.log("jasiik")
-    // Inisialisasi peta
-    const map = L.map('map').setView([51.505, -0.09], 13);
+    ngOnInit(): void {
+        this.search()
+        // Inisialisasi peta
+        const map = L.map('map').setView([51.505, -0.09], 13);
 
-    // Tambahkan tile layer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+        // Tambahkan tile layer
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
 
-    // Tambahkan marker
-    L.marker([51.5, -0.09]).addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
-  }
+        // Tambahkan marker
+        L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    }
+
+    search() {
+        const params = {
+            q: "Jl Puter",
+            format: 'json',
+            addressdetails: '1',
+            limit: '5'
+        };
+    
+        this.globalService.DataApi('https://nominatim.openstreetmap.org/search', { params }).subscribe((data: any) => {
+            this.results = data;
+        });
+      }
 
 
 }
