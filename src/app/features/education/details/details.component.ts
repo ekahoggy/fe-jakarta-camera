05e844ee8e4a0b742cf053776c50873b8fd36edd
Galih Satrio Wibisono <sgalih1234@gmail.com>
2024-06-26 11:13:36 +0700

    profile bahasa indonesia

--- src/app/features/education/details/details.component.ts
+++ src/app/features/education/details/details.component.ts
@@ -5,55 +5,58 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { GlobalService } from 'src/app/services/global.service';
 
 @Component({
-  selector: 'app-details',
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+    selector: 'app-details',
+    templateUrl: './details.component.html',
+    styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
-  model: any = [];
-  detail: any = [];
-  listCategory: any = [];
-  urlSafe: any;
-  loadingPage: boolean = true;
+    model: any = [];
+    detail: any = [];
+    listCategory: any = [];
+    urlSafe: any;
+    loadingPage: boolean = true;
+    play: boolean = false;
+    activeTab: string = 'deskripsi';
 
-  constructor(
-    private globalService: GlobalService,
-    private route: ActivatedRoute,
-    private modalService: NgbModal,
-    public sanitizer: DomSanitizer,
-  ) { }
+    constructor(
+        private globalService: GlobalService,
+        private route: ActivatedRoute,
+        private modalService: NgbModal,
+        public sanitizer: DomSanitizer,
+    ) { }
 
-  ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      const slug = params['slug'];
-      this.getDataBySlug(slug);
-    });
-  }
-
-  getDataBySlug(slug){
-    this.loadingPage = true;
-    this.globalService.DataGet(`/public/edukasi/${slug}`).subscribe((res: any) => {
-      this.model = res.data;
-      this.detail = res.detail;
-    })
-  }
-
-  openModal(modal: TemplateRef<any>, is_lock) {
-    if(is_lock === 1){
-      this.alertSubscribe()
+    ngOnInit(): void {
+        this.route.params.subscribe(params => {
+            const slug = params['slug'];
+            this.getDataBySlug(slug);
+        });
     }
-    else{
-      this.modalService.open(modal, { size: 'xl', backdrop: 'static' });
-      this.urlSafe = this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/sVNFrxLW2pA");
+
+    getDataBySlug(slug) {
+        this.loadingPage = true;
+        this.globalService.DataGet(`/public/edukasi/${slug}`).subscribe((res: any) => {
+            this.model = res.data;
+            this.detail = res.detail;
+            this.urlSafe = this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/sVNFrxLW2pA");
+        })
     }
-  }
+
+    openModal(modal: TemplateRef<any>, is_lock) {
+        if (is_lock === 1) {
+            this.alertSubscribe()
+        }
+        else {
+            this.modalService.open(modal, { size: 'xl', backdrop: 'static' });
+            this.urlSafe = this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/sVNFrxLW2pA");
+        }
+    }
 
 
-  alertSubscribe() {
-    this.globalService.alertQuestion("Anda belum berlangganan", "Untuk mengakses konten edukasi ini, Anda harus berlangganan terlebih dahulu.", 3000);
-  }
+    alertSubscribe() {
+        this.globalService.alertQuestion("Anda belum berlangganan", "Untuk mengakses konten edukasi ini, Anda harus berlangganan terlebih dahulu.", 3000);
+    }
 
-  openModalSubscribe(modal: TemplateRef<any>) {
-    this.modalService.open(modal, { size: 'md', backdrop: 'static' });
-  }
+    openModalSubscribe(modal: TemplateRef<any>) {
+        this.modalService.open(modal, { size: 'md', backdrop: 'static' });
+    }
 }
