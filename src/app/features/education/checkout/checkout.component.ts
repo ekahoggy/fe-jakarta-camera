f702eae7ca6a18e2b8ec14dbd70f3f5a059cb63c
hoggykurnia@gmail.com <hoggykurnia@gmail.com>
2024-06-28 06:16:53 +0700

    checkout pengiriman

--- src/app/features/education/checkout/checkout.component.ts
+++ src/app/features/education/checkout/checkout.component.ts
@@ -12,8 +12,11 @@ export class CheckoutComponent implements OnInit {
   loadingPage: boolean = true;
   model: any = {};
   detailCheckout: any = {};
-  totalEdukasi: number= 0;
+  totalEdukasi: number = 0;
   approveSNK = false;
+  loading = false;
+  userId: string = "";
+  session: any = {};
 
   constructor(
     private globalService: GlobalService,
@@ -23,34 +26,57 @@ export class CheckoutComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.session = this.globalService.getAuth()['user'];
+    this.model.email = this.session.email;
+    this.userId = this.session.id
     this.route.params.subscribe(params => {
       const slug = params['slug'];
       this.getDataBySlug(slug);
-
     });
   }
 
   getDataBySlug(slug) {
     this.loadingPage = true;
-    this.globalService.DataGet(`/public/edukasi/${slug}`).subscribe((res: any) => {
+    this.globalService.DataGet(`/public/edukasi/` + slug).subscribe((res: any) => {
       this.model = res.data;
       this.hitungCheckout()
     })
   }
 
-  getVoucher(){
+  getVoucher() {
 
   }
 
-  hitungCheckout(){
+  hitungCheckout() {
     this.detailCheckout.harga = this.model.harga
     this.detailCheckout.promo = 0;
     this.detailCheckout.voucher = 0;
     this.detailCheckout.total = this.detailCheckout.harga - this.detailCheckout.promo - this.detailCheckout.voucher;
-    console.log(this.detailCheckout);
+  }
+
+  pay() {
+    this.model.user_id = this.userId;
+    let params = {
+      data: this.model,
+      checkout: this.detailCheckout
+    };
+    this.globalService.DataPost(`/public/edukasi/pay`, params).subscribe((res: any) => {
+      this.loading = false;
+      this.globalService.alertSuccess("Success", "Success create order")
+      this.navigatePembayaran(res.data.payment_link)
+    })
   }
 
   openVoucher(content: TemplateRef<any>) {
     this.offcanvasService.open(content, { position: 'bottom' });
+  }
+
+  navigatePembayaran(url: string) {
+    const param = {
+      'url': url,
+    };
+    const data = btoa(JSON.stringify(param));
+    this.loading = false
+    this.router.navigate(['/payment'], { queryParams: { 'data': data } });
   }
 }
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -6,322 +6,330 @@ SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 import * as AOS from 'aos';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 
 export class HomeComponent implements OnInit {
-    private navbar: HTMLElement | null = null;
-    private sticky: number = 0;
-    carousel: HTMLElement | null = null;
-    configSliders: SwiperOptions = {};
-    configFeatured: SwiperOptions = {};
-    configBrand: SwiperOptions = {};
-    configFlash: SwiperOptions = {};
-    configCategory: SwiperOptions = {};
-    listSlider: any = [];
-    listProduct: any = [];
-    listProductPromo: any = [];
-    listProductFlashsale: any = [];
-    listCategory: any = [];
-    statusPopup: boolean = true;
-    listNews: any = [];
-    listPopup: any = [];
-    loading = {
-        slider: false,
-        product: false,
-        category: false,
+  private navbar: HTMLElement | null = null;
+  private sticky: number = 0;
+  carousel: HTMLElement | null = null;
+  configSliders: SwiperOptions = {};
+  configFeatured: SwiperOptions = {};
+  configBrand: SwiperOptions = {};
+  configFlash: SwiperOptions = {};
+  configCategory: SwiperOptions = {};
+  listSlider: any = [];
+  listProduct: any = [];
+  listProductPromo: any = [];
+  listProductFlashsale: any = [];
+  listCategory: any = [];
+  statusPopup: boolean = true;
+  listNews: any = [];
+  listPopup: any = [];
+  loading = {
+    slider: false,
+    product: false,
+    category: false,
+  };
+  aktifPopup: boolean = false;
+  filterProduct: string = 'featured';
+
+  constructor(
+    private globalService: GlobalService,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+    this.init();
+    AOS.init();
+    this.getSettingPopUp();
+    this.getSlider();
+    this.getCategories();
+    this.getProduct();
+    this.getProductPromo();
+    this.configSlider();
+    this.configFaturedProduct();
+    this.configSwiperBrand();
+    this.configSwiperFlash();
+    this.configSwiperCategory();
+
+    this.getNews();
+    this.carousel = document.getElementById("banner-carousel");
+    if (this.navbar) {
+      this.sticky = this.navbar.offsetTop - 20;
+    }
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    this.myFunction();
+  }
+
+  init() {
+    this.loading = {
+      slider: false,
+      product: false,
+      category: false,
     };
-    aktifPopup: boolean = false;
-    filterProduct: string = 'featured';
+  }
 
-    constructor(
-        private globalService: GlobalService,
-        private router: Router,
-    ) { }
-
-    ngOnInit() {
-        this.init();
-        AOS.init();
-        this.getSettingPopUp();
-        this.getSlider();
-        this.getCategories();
-        this.getProduct();
-        this.getProductPromo();
-        this.configSlider();
-        this.configFaturedProduct();
-        this.configSwiperBrand();
-        this.configSwiperFlash();
-        this.configSwiperCategory();
-
-        this.getNews();
-        this.carousel = document.getElementById("banner-carousel");
-        if (this.navbar) {
-            this.sticky = this.navbar.offsetTop - 20;
-        }
+  private myFunction() {
+    if (this.carousel && window.scrollY >= this.sticky) {
+      this.carousel.classList.add("content-up");
+    } else if (this.carousel) {
+      this.carousel.classList.remove("content-up");
     }
+  }
 
-    @HostListener('window:scroll', [])
-    onScroll() {
-        this.myFunction();
+  onSwiper(swiper: any) {
+    // console.log(swiper);
+  }
+
+  onSlideChange() {
+    console.log('slide change');
+  }
+
+  configSlider() {
+    this.configSliders = {
+      navigation: true,
     }
+  }
 
-    init() {
-        this.loading = {
-            slider: false,
-            product: false,
-            category: false,
-        };
+  configSwiperCategory() {
+    this.configCategory = {
+      slidesPerView: 6,
+      spaceBetween: 20,
+      navigation: true,
+      pagination: false,
+      scrollbar: { draggable: true },
+      breakpoints: {
+        100: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 15,
+        },
+        1400: {
+          slidesPerView: 6,
+          spaceBetween: 15,
+        },
+      },
     }
+  }
 
-    private myFunction() {
-        if (this.carousel && window.scrollY >= this.sticky) {
-            this.carousel.classList.add("content-up");
-        } else if (this.carousel) {
-            this.carousel.classList.remove("content-up");
-        }
+  configSwiperFlash() {
+    this.configFlash = {
+      slidesPerView: 5,
+      spaceBetween: 20,
+      navigation: true,
+      pagination: false,
+      scrollbar: { draggable: true },
+      breakpoints: {
+        100: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 15,
+        },
+        1400: {
+          slidesPerView: 5,
+          spaceBetween: 15,
+        },
+      },
     }
+  }
 
-    onSwiper(swiper: any) {
-        // console.log(swiper);
+  configFaturedProduct() {
+    this.configFeatured = {
+      slidesPerView: 6,
+      spaceBetween: 15,
+      navigation: true,
+      pagination: false,
+      scrollbar: { draggable: true },
+      breakpoints: {
+        100: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 15,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 15,
+        },
+        1400: {
+          slidesPerView: 6,
+          spaceBetween: 15,
+        },
+      },
     }
+  }
 
-    onSlideChange() {
-        console.log('slide change');
+  configSwiperBrand() {
+    this.configBrand = {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      navigation: false,
+      pagination: false,
+      scrollbar: { draggable: true },
+      breakpoints: {
+        100: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        480: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 15,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 15,
+        },
+        1400: {
+          slidesPerView: 6,
+          spaceBetween: 15,
+        },
+      },
     }
+  }
 
-    configSlider() {
-        this.configSliders = {
-            navigation: true,
-        }
-    }
+  getSlider() {
+    this.loading.slider = true;
+    this.globalService.DataGet('/public/slider').subscribe((res: any) => {
+      this.listSlider = res.data.list;
+      this.loading.slider = false;
+    })
+  }
 
-    configSwiperCategory() {
-        this.configCategory = {
-            slidesPerView: 6,
-            spaceBetween: 20,
-            navigation: true,
-            pagination: false,
-            scrollbar: { draggable: true },
-            breakpoints: {
-                100: {
-                    slidesPerView: 2,
-                    spaceBetween: 15,
-                },
-                480: {
-                    slidesPerView: 3,
-                    spaceBetween: 15,
-                },
-                768: {
-                    slidesPerView: 2,
-                    spaceBetween: 15,
-                },
-                1024: {
-                    slidesPerView: 4,
-                    spaceBetween: 15,
-                },
-                1400: {
-                    slidesPerView: 6,
-                    spaceBetween: 15,
-                },
-            },
-        }
-    }
-
-    configSwiperFlash() {
-        this.configFlash = {
-            slidesPerView: 5,
-            spaceBetween: 20,
-            navigation: true,
-            pagination: false,
-            scrollbar: { draggable: true },
-            breakpoints: {
-                100: {
-                    slidesPerView: 2,
-                    spaceBetween: 15,
-                },
-                480: {
-                    slidesPerView: 3,
-                    spaceBetween: 15,
-                },
-                768: {
-                    slidesPerView: 2,
-                    spaceBetween: 15,
-                },
-                1024: {
-                    slidesPerView: 4,
-                    spaceBetween: 15,
-                },
-                1400: {
-                    slidesPerView: 5,
-                    spaceBetween: 15,
-                },
-            },
-        }
-    }
-
-    configFaturedProduct() {
-        this.configFeatured = {
-            slidesPerView: 6,
-            spaceBetween: 15,
-            navigation: true,
-            pagination: false,
-            scrollbar: { draggable: true },
-            breakpoints: {
-                100: {
-                    slidesPerView: 2,
-                    spaceBetween: 15,
-                },
-                480: {
-                    slidesPerView: 2,
-                    spaceBetween: 15,
-                },
-                768: {
-                    slidesPerView: 4,
-                    spaceBetween: 15,
-                },
-                1024: {
-                    slidesPerView: 5,
-                    spaceBetween: 15,
-                },
-                1400: {
-                    slidesPerView: 6,
-                    spaceBetween: 15,
-                },
-            },
-        }
-    }
-
-    configSwiperBrand() {
-        this.configBrand = {
-            slidesPerView: 4,
-            spaceBetween: 20,
-            navigation: false,
-            pagination: false,
-            scrollbar: { draggable: true },
-            breakpoints: {
-                100: {
-                    slidesPerView: 2,
-                    spaceBetween: 15,
-                },
-                480: {
-                    slidesPerView: 3,
-                    spaceBetween: 15,
-                },
-                768: {
-                    slidesPerView: 4,
-                    spaceBetween: 15,
-                },
-                1024: {
-                    slidesPerView: 5,
-                    spaceBetween: 15,
-                },
-                1400: {
-                    slidesPerView: 6,
-                    spaceBetween: 15,
-                },
-            },
-        }
-    }
-
-    getSlider() {
-        this.loading.slider = true;
-        this.globalService.DataGet('/public/slider').subscribe((res: any) => {
-            this.listSlider = res.data.list;
-            this.loading.slider = false;
-        })
-    }
-
-    getSettingPopUp() {
-        this.globalService.DataGet('/public/settingPopup', { kategori: 'W' }).subscribe((res: any) => {
-            res.data.forEach(setting => {
-                if (setting.name === 'popup') {
-                    if (setting.value === 'YES') {
-                        this.aktifPopup = true;
-                        this.globalService.DataGet('/public/popup').subscribe((response: any) => {
-                            this.listPopup = response.data;
-                        });
-                    }
-                    else {
-                        this.aktifPopup = false;
-                    }
-                }
+  getSettingPopUp() {
+    this.globalService.DataGet('/public/setting', { kategori: 'W' }).subscribe((res: any) => {
+      res.data.forEach(setting => {
+        if (setting.name === 'popup') {
+          if (setting.value === 'YES') {
+            this.aktifPopup = true;
+            this.globalService.DataGet('/public/popup').subscribe((response: any) => {
+              this.listPopup = response.data;
             });
-        })
-    }
-
-    popupClick(url: string) {
-        if (url !== null || url !== '') {
-            window.open(url)
+          }
+          else {
+            this.aktifPopup = false;
+          }
         }
-    }
+      });
+    })
+  }
 
-    sliderClick(url: string) {
-        if (url !== null || url !== '') {
-            window.open(url, '_blank')
-        }
+  popupClick(url: string) {
+    if (url !== null || url !== '') {
+      window.open(url)
     }
+  }
 
-    changeTipeProduct(tipe: string) {
-        this.filterProduct = tipe;
+  sliderClick(url: string) {
+    if (url !== null || url !== '') {
+      window.open(url, '_blank')
     }
+  }
 
-    getProduct() {
-        this.loading.product = true;
-        this.globalService.DataGet('/public/produk').subscribe((res: any) => {
-            this.listProduct = res.data.list;
-            this.loading.product = false;
-        })
-    }
+  changeTipeProduct(tipe: string) {
+    this.filterProduct = tipe;
+  }
 
-    getProductPromo() {
-        this.loading.product = true;
-        this.globalService.DataGet('/public/produkPromo').subscribe((res: any) => {
-            this.listProductPromo = res.data;
-            this.loading.product = false;
-        })
-    }
+  getProduct() {
+    this.loading.product = true;
+    this.globalService.DataGet('/public/produk').subscribe((res: any) => {
+      this.listProduct = res.data.list;
+      this.loading.product = false;
+    })
+  }
 
-    getCategories() {
-        this.loading.category = true;
-        this.globalService.DataGet('/public/kategori').subscribe((res: any) => {
-            this.listCategory = res.data;
-            this.loading.category = false;
-        })
-    }
+  getProductPromo() {
+    this.loading.product = true;
+    this.globalService.DataGet('/public/produkPromo').subscribe((res: any) => {
+      this.listProductPromo = res.data;
+      this.loading.product = false;
+    })
+  }
 
-    clickProduk(item) {
-        this.router.navigate([`/katalog/detail/` + item.slug]);
-        this.globalService.productToLocalStorage(item);
-    }
+  getProductFlashsale() {
+    this.loading.product = true;
+    this.globalService.DataGet('/public/flashsale').subscribe((res: any) => {
+      this.listProductFlashsale = res.data;
+      this.loading.product = false;
+    })
+  }
 
-    clickViewsNews(item) {
-        this.globalService.clickToViewsNews(item);
-    }
+  getCategories() {
+    this.loading.category = true;
+    this.globalService.DataGet('/public/kategori').subscribe((res: any) => {
+      this.listCategory = res.data;
+      this.loading.category = false;
+    })
+  }
 
-    closePopup() {
-        this.statusPopup = false;
-    }
+  clickProduk(item) {
+    this.router.navigate([`/katalog/detail/` + item.slug]);
+    this.globalService.productToLocalStorage(item);
+  }
 
-    addCart(productId: string) {
-        if (this.globalService.getAuth() === null) {
-            this.router.navigate(['/login']);
-        } else {
-            let params = {
-                user_id: this.globalService.getAuth()['user']['id'],
-                product_id: productId,
-                quantity: 1
-            }
-            this.globalService.DataPost('/cart/add', params).subscribe((res: any) => {
-                this.globalService.alertSuccess('Success', res.message);
-            })
-        }
-    }
+  clickViewsNews(item) {
+    this.globalService.clickToViewsNews(item);
+  }
 
-    getNews() {
-        this.globalService.DataGet('/public/news').subscribe((res: any) => {
-            this.listNews = res.data.list;
-        })
+  closePopup() {
+    this.statusPopup = false;
+  }
+
+  addCart(productId: string) {
+    if (this.globalService.getAuth() === null) {
+      this.router.navigate(['/login']);
+    } else {
+      let params = {
+        user_id: this.globalService.getAuth()['user']['id'],
+        product_id: productId,
+        quantity: 1
+      }
+      this.globalService.DataPost('/cart/add', params).subscribe((res: any) => {
+        this.globalService.alertSuccess('Success', res.message);
+      })
     }
+  }
+
+  getNews() {
+    this.globalService.DataGet('/public/news').subscribe((res: any) => {
+      this.listNews = res.data.list;
+    })
+  }
 }
