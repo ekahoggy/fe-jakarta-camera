beb8eadfbe48ad3a5bec3aa597c50217857169b7
hoggy <hoggykurnia@gmail.com>
2024-08-07 16:05:55 +0700

    environtment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
-let url = 'https://jakartacamera.moodstudio.id/'
-let apiURL = 'https://apijakartacamera.moodstudio.id/api/v1';
-let api = 'https://apijakartacamera.moodstudio.id';
-let originalImageURL = 'https://apijakartacamera.moodstudio.id/img/media/originals/';
-let productImageURL = 'https://apijakartacamera.moodstudio.id/img/media/product/';
+let url = 'https://dev.jakartacamera.com/'
+let apiURL = 'https://apidev.jakartacamera.com/api/v1';
+let api = 'https://apidev.jakartacamera.com';
+let originalImageURL = 'https://apidev.jakartacamera.com/img/media/originals/';
+let productImageURL = 'https://apidev.jakartacamera.com/img/media/product/';
 let googleIdClient = '1076513397291-ic5p0c2ohngbn3fa0159d2tej9s6qkco.apps.googleusercontent.com';
 
 if (window.location.hostname == 'localhost') {
@@ -11,6 +11,14 @@ if (window.location.hostname == 'localhost') {
   apiURL = 'http://localhost:8000/api/v1';
   originalImageURL = 'http://localhost:8000/img/media/originals/';
   productImageURL = 'http://localhost:8000/img/media/product/';
+}
+
+if (window.location.hostname == 'moodstudio.id') {
+  url = 'https://jakartacamera.moodstudio.id/'
+  api = 'https://apijakartacamera.moodstudio.id';
+  apiURL = 'https://apijakartacamera.moodstudio.id/api/v1';
+  originalImageURL = 'https://apijakartacamera.moodstudio.id/img/media/originals/';
+  productImageURL = 'https://apijakartacamera.moodstudio.id/img/media/product/';
 }
 
 export const environment = {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,23 +1,33 @@
-let url = 'https://jakartacamera.moodstudio.id/'
-let apiURL = 'https://apijakartacamera.moodstudio.id/api/v1';
-let api = 'https://apijakartacamera.moodstudio.id';
-let originalImageURL = 'https://apijakartacamera.moodstudio.id/img/media/originals/';
-let productImageURL = 'https://apijakartacamera.moodstudio.id/img/media/product/';
+let url = 'https://new.jakartacamera.com/'
+let apiURL = 'https://api.jakartacamera.com/api/v1';
+let api = 'https://api.jakartacamera.com';
+let originalImageURL = 'https://api.jakartacamera.com/img/media/originals/';
+let productImageURL = 'https://api.jakartacamera.com/img/media/product/';
 let googleIdClient = '1076513397291-ic5p0c2ohngbn3fa0159d2tej9s6qkco.apps.googleusercontent.com';
 
 if (window.location.hostname == 'localhost') {
   url = 'http://localhost:3200/'
-  apiURL = 'http://127.0.0.1:8000/api/v1';
-  api = 'http://127.0.0.1:8000';
-  originalImageURL = 'http://127.0.0.1:8000/img/media/originals/';
-  productImageURL = 'http://127.0.0.1:8000/img/media/product/';
+  api = 'http://localhost:8000';
+  apiURL = 'http://localhost:8000/api/v1';
+  originalImageURL = 'http://localhost:8000/img/media/originals/';
+  productImageURL = 'http://localhost:8000/img/media/product/';
+}
+
+if (window.location.hostname == 'moodstudio.id') {
+  url = 'https://jakartacamera.moodstudio.id/'
+  api = 'https://apijakartacamera.moodstudio.id';
+  apiURL = 'https://apijakartacamera.moodstudio.id/api/v1';
+  originalImageURL = 'https://apijakartacamera.moodstudio.id/img/media/originals/';
+  productImageURL = 'https://apijakartacamera.moodstudio.id/img/media/product/';
 }
 
 export const environment = {
   url: url,
-  apiURL: apiURL,
   api: api,
+  apiURL: apiURL,
   googleIdClient: googleIdClient,
   originalImageURL: originalImageURL,
   productImageURL: productImageURL,
+
+  version: 1.0
 };
