100b0244e08a487264ad6cac5881f52b92875224
galihsatriowibisono <sgalih1234@gmail.com>
2024-08-18 23:19:46 +0700

    -

--- src/app/features/education/education.module.ts
+++ src/app/features/education/education.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { EducationRoutingModule } from './education-routing.module';
-import { ListComponent } from './list/list.component';
-import { DetailsComponent } from './details/details.component';
-import { CheckoutComponent } from './checkout/checkout.component';
-import { CartComponent } from './cart/cart.component';
-import { NgbDropdownModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LayoutsModule } from 'src/app/layouts/layouts.module';
 import { SwiperModule } from 'swiper/angular';
 import { FormsModule } from '@angular/forms';
 import { CountdownModule } from 'ngx-countdown';
 import { CoreModule } from 'src/app/core/core.module';
+import { NgbDropdownModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { EducationRoutingModule } from './education-routing.module';
+import { LayoutsModule } from 'src/app/layouts/layouts.module';
+
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CartComponent } from './cart/cart.component';
 
 
 @NgModule({
@@ -22,15 +22,15 @@ import { CoreModule } from 'src/app/core/core.module';
     CartComponent
   ],
   imports: [
-      CommonModule,
-      EducationRoutingModule,
-      NgbDropdownModule,
-      NgbModule,
-      LayoutsModule,
-      SwiperModule,
-      FormsModule,
-      CountdownModule,
-      CoreModule
+    CommonModule,
+    EducationRoutingModule,
+    NgbDropdownModule,
+    NgbModule,
+    LayoutsModule,
+    SwiperModule,
+    FormsModule,
+    CountdownModule,
+    CoreModule
   ]
 })
 export class EducationModule { }
